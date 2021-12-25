23a616c30d32cfa9d3a34f3d3fd7a4fb0b619245
K1tan <76427041+K1tan@users.noreply.github.com>
2021-12-25 23:50:02 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,1 @@
-set M2_HOME=C:\Program Files\apache-maven-3.8.4\apache-maven;
-set path=C:\Program Files\apache-maven-3.8.4\apache-maven\bin;%path%;
-set JAVA_HOME=C:\Program Files\Java\jdk-11.0.13;
-set path=C:\Program Files\Java\jdk-11.0.13\bin;%path%;
-mvn clean package
+./mvnw package
